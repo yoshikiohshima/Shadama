0aebf9e63524ea7560e14aa372ff46e2a7c484ad
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2017-04-03 14:16:36 -0700

    comparison between pure JS vs GL.

--- shadama.js
+++ shadama.js
@@ -17,6 +17,9 @@ var indicesIBO;
 
 var myBreed;
 
+var frames;
+var diffTime;
+
 function initIndices(gl) {
     allIndices = new Array(65536);
     for (var i = 0; i < 65536; i++) {
@@ -81,8 +84,6 @@ function createTexture(gl, imageData) {
     gl.bindTexture(gl.TEXTURE_2D, tex);
 			
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
-			
-//    gl.generateMipmap(gl.TEXTURE_2D);
 			
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -180,8 +181,20 @@ function Breed(gl, count, color) {
 };
 
 function step() {
+    frames++;
+    var sTime = Date.now();
+
     myBreed.render(gl);
     myBreed.forward(gl, 3.0);
+
+//    debugDisplay1(gl, myBreed);
+
+    diffTime += (Date.now() - sTime);
+    if (frames % 60 === 0) {
+	console.log(diffTime / 60.0);
+	diffTime = 0;
+    }
+
     window.requestAnimationFrame(step);
 }
 
@@ -205,6 +218,9 @@ onload = function() {
     programs['forward'] = forwardProgram(gl);
 
     myBreed = new Breed(gl, 32768, [1.0, 0.0, 0.0, 1.0]);
+
+    frames = 0;
+    diffTime = 0;
 
     window.requestAnimationFrame(step);
 };
@@ -349,131 +365,4 @@ function debugDisplay2(gl, breed) {
     gl.readPixels(0, 0, 256, 256, gl.RGBA, gl.UNSIGNED_BYTE, debugArray);
     var img = new ImageData(new Uint8ClampedArray(debugArray.buffer), 256, 256);
     debugCanvas2.getContext('2d').putImageData(img, 0, 0);
-};
-
-function makePos() {
-    var ary = new Uint8ClampedArray(256*256*4);
-    for (var j = 0; j < 256; j++) {
-	for (var i = 0; i < 256; i++) {
-	    var ind = (j * 256 + i) * 4;
-	    ary[ind + 0] = i;
-	    ary[ind + 1] = 0;
-	    ary[ind + 2] = j;
-	    ary[ind + 3] = 255;
-	}
-    }
-    return new ImageData(ary, 256, 256);
-};
-
-function makeDest() {
-    var ary = new Uint8ClampedArray(256*256*4);
-    return new ImageData(ary, 256, 256);
-};
-
-function render(image, destImage) {
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, "copy.vert");
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, "copy.frag");
-    var program = createProgram(gl, vertexShader, fragmentShader);
-
-    var attrLocations = [];
-    attrLocations.push(gl.getAttribLocation(program, "a_position"));
-//    attrLocations.push(gl.getAttribLocation(program, "a_texCoord"));
-
-    var uniLocations = {};
-    uniLocations["u_resolution"] = gl.getUniformLocation(program, "u_resolution");
-    uniLocations["u_image"] = gl.getUniformLocation(program, "u_image");
-    uniLocations["u_dest"] = gl.getUniformLocation(program, "u_dest");
-
-    // Create a vertex array object (attribute state)
-    var vao = gl.createVertexArray();
-
-    // and make it the one we're currently working with
-    gl.bindVertexArray(vao);
-
-    var indices = new Array(65536);
-    for (var i = 0; i < 65536; i++) {
-	indices[i] = i;
-    }
-
-    var vertices = new Array(65536 * 2);
-    for (var j = 0; j < 256; j++) {
-	for (i = 0; i < 256; i++) {
-	    var ind = (j * 256 + i) * 2;
-	    vertices[ind + 0] = i;
-	    vertices[ind + 1] = j;
-	}
-    }
-
-    var ibo = createIBO(gl, indices);
-    
-    var positionBuffer = gl.createBuffer();
-//    var texCoordBuffer = gl.createBuffer();
-
-    set_buffer_attribute(gl,
-			 [positionBuffer],
-			 [vertices
-//			     [0, 0, 256, 0, 0, 256,256, 256],
-			     //    [0.0,  0.0, 1.0,  0.0, 0.0,  1.0, 1.0,  1.0]
-			 ],
-			 attrLocations, [2]);
-
-    var texture = gl.createTexture();
-    src = texture;
-
-    gl.activeTexture(gl.TEXTURE0);
- 
-    // Bind it to texture unit 0' 2D bind point
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    // Set the parameters so we don't need mips and so we're not filtering
-    // and we don't repeat
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-    // Upload the image into the texture.
-    var mipLevel = 0;               // the largest mip
-    var internalFormat = gl.RGBA;   // format we want in the texture
-    var srcFormat = gl.RGBA;        // format of data we are supplying
-    var srcType = gl.UNSIGNED_BYTE  // type of data we are supplying
-    gl.texImage2D(gl.TEXTURE_2D,
-                  mipLevel,
-                  internalFormat,
-                  srcFormat,
-                  srcType,
-                  image);
-
-    var destTexture = gl.createTexture();
-    dst = destTexture;
-    gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, destTexture);
-
-    framebuffer = gl.createFramebuffer();
-    initFramebuffer(gl, framebuffer, destTexture);
-
-
-//    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    // Clear the canvas
-    gl.clearColor(0, 0.2, 0.2, 0.2);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Tell it to use our program (pair of shaders)
-    gl.useProgram(program);
-
-    // Bind the attribute/buffer set we want.
-    gl.bindVertexArray(vao);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-
-    gl.uniform2f(uniLocations["u_resolution"], gl.canvas.width, gl.canvas.height);
-    gl.uniform1i(uniLocations["u_image"], 0);
-
-    gl.drawElements(gl.POINTS, 20000, gl.UNSIGNED_SHORT, 0);
-//    gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-    gl.flush();
-
 };
---
+++ shadamajs.js
@@ -0,0 +1,121 @@
+var src;
+var dst;
+
+var debugCanvas1;
+var debugCanvas2;
+
+var debugArray;
+
+var programs = {};
+
+var allIndices;
+var vertices;
+var indicesIBO;
+
+var myBreed;
+
+var frames;
+var diffTime;
+
+function randomDirection() {
+  return Math.random() * Math.PI * 2.0;
+};
+
+function Breed(count, color) {
+
+    this.pos = new Float32Array(65536 * 2);
+    for (var j = 0; j < 256; j++) {
+	for (var i = 0; i < 256; i++) {
+	    var ind = (j * 256 + i) * 2;
+	    this.pos[ind + 0] = i;
+	    this.pos[ind + 1] = j;
+	}
+    }
+
+    this.newPos = new Float32Array(65536 * 2);
+
+    this.dir = new Float32Array(65536);
+    for (var i = 0; i < 256 * 256; i++) {
+	var d = randomDirection();
+	this.dir[i] = d;
+    }
+
+    this.count = count;
+    this.color = color.map(function(x) {return Math.floor(x * 256);});
+};
+
+function step() {
+    frames++;
+    var sTime = Date.now();
+
+
+    myBreed.render();
+    myBreed.forward(3.0);
+
+    diffTime += (Date.now() - sTime);
+    if (frames % 60 === 0) {
+	console.log(diffTime / 60.0);
+	diffTime = 0;
+    }
+    window.requestAnimationFrame(step);
+}
+
+onload = function() {
+    var c = document.getElementById('canvas');
+    c.width = 256;
+    c.height = 256;
+    
+    myBreed = new Breed(32768, [1.0, 0.0, 0.0, 1.0]);
+
+    frames = 0;
+    diffTime = 0;
+    window.requestAnimationFrame(step);
+};
+
+Breed.prototype.render = function() {
+    var cxt = document.getElementById('canvas').getContext('2d');
+
+    cxt.clearRect(0, 0, 256, 256);
+    var imageData = cxt.getImageData(0, 0, 256, 256);
+    var dest = imageData.data;
+
+    for (var i = 0; i < this.count; i++) {
+	var inX = this.pos[i * 2];
+	var inY = this.pos[i * 2 + 1];
+
+	var outBase = Math.floor(inY * 256 + inX) * 4;
+
+//	console.log(inX, inY, outBase);
+	
+	dest[outBase + 0] = this.color[0];
+	dest[outBase + 1] = this.color[1];
+	dest[outBase + 2] = this.color[2];
+	dest[outBase + 3] = this.color[3];
+    }
+    cxt.putImageData(imageData, 0, 0);
+};
+
+Breed.prototype.forward = function(amount) {
+    for (var i = 0; i < this.count; i++) {
+	var inX = this.pos[i * 2];
+	var inY = this.pos[i * 2 + 1];
+	var dir = this.dir[i];
+	
+	var cos = Math.cos(dir);
+	var sin = Math.sin(dir);
+	var newX = inX + cos * amount;
+	var newY = inY + sin * amount;
+	
+	if (newX > 256.0) {newX = newX - 256.0;}
+	if (newX < 0.0) {newX = newX + 256.0;}
+	if (newY > 256.0) {newY = newY - 256.0;}
+	if (newY < 0.0) {newY = newY + 256.0;}
+	
+	this.newPos[i * 2] = newX;
+	this.newPos[i * 2 + 1] = newY;
+    }
+
+    var tmp = this.pos;
+    this.pos = this.newPos;
+    this.newPos = tmp;
+};
