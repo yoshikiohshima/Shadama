23f0869f303085d61cfa18d79304ea501622eebb
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2019-02-24 05:32:04 +0900

    stop using createObjectURL

--- shadama.js
+++ shadama.js
@@ -1819,7 +1819,7 @@ function ShadamaFactory(frame, optDimension, parent, optDefaultProgName, optDOMT
             if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                 // Not adding `{ audio: true }` since we only want video now
                 navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-                    video.src = window.URL.createObjectURL(stream);
+                    video.srcObject = stream; //window.URL.createObjectURL(stream);
                     video.play();
                 });
             }
